0ca5f59f8cffb1cf2c48aadfd64fbb2fb1587915
9900K <46859140+9900K@users.noreply.github.com>
2019-01-28 19:46:31 -0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -8,19 +8,16 @@ module.exports =
   VERSION:        packageJson.version,
   CLIENT_SEED:
     process.env.BUSTABIT_CLIENTSEED ||
-    '0000000000000000004d6ec16dafe9d8370958664c1dc422f452892264c59526',
-  API_SERVER:     process.env.BUSTABIT_API_SERVER || "wss://bb.apservices.bz/ws",
+    '60567ee7538baa047511a7e3d20137a5b763a8b2bfd3be5412f88ca9c349f06a',
+  API_SERVER:     process.env.BUSTABIT_API_SERVER || "http://185.141.61.20:3841/",
   API_KEY:        process.env.BUSTABIT_API_KEY,
-  OXR_APP_ID:     process.env.OXR_APP_ID,
-  SESSION:        process.env.SHIBA_SESSION,
+  OXR_APP_ID:     process.env.OXR_APP_ID
+  SESSION:        process.env.SHIBA_SESSION || '79df49ed-7113-4201-b3bf-5c8b2388495d'
   DATABASE:       process.env.SHIBA_DATABASE || 'postgres://localhost/shibadb',
   CHAT_HISTORY:   process.env.SHIBA_CHAT_HISTORY || 2000,
   GAME_HISTORY:   process.env.SHIBA_GAME_HISTORY || 200,
   /* keep in lowercase */
   USER_WHITELIST: [
-    "daniel",
-    "kungfuant",
-    "ryan",
-    "steve",
+    "enzo",
   ]
 };
