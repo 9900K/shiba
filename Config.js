4c2080d24113bab61f74631587c0fc304f27ea27
9900K <46859140+9900K@users.noreply.github.com>
2019-01-28 20:54:21 -0700

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,23 @@
+
+let packageJson = require('./package.json');
+
+/* eslint no-process-env: 0 */
+module.exports =
+{
+  ENV:            process.env.NODE_ENV || 'development',
+  VERSION:        packageJson.version,
+  CLIENT_SEED:
+    process.env.BUSTABIT_CLIENTSEED ||
+    '60567ee7538baa047511a7e3d20137a5b763a8b2bfd3be5412f88ca9c349f06a',
+  API_SERVER:     process.env.BUSTABIT_API_SERVER || "http://185.141.61.20:3841/",
+  API_KEY:        process.env.BUSTABIT_API_KEY,
+  OXR_APP_ID:     process.env.OXR_APP_ID,
+  SESSION:        process.env.SHIBA_SESSION || '871c47c0-67fc-4eda-96b6-5dd8de992a4c',
+  DATABASE:       process.env.SHIBA_DATABASE || 'postgres://localhost/shibadb',
+  CHAT_HISTORY:   process.env.SHIBA_CHAT_HISTORY || 2000,
+  GAME_HISTORY:   process.env.SHIBA_GAME_HISTORY || 200,
+  /* keep in lowercase */
+  USER_WHITELIST: [
+    "enzo",
+  ]
+};
